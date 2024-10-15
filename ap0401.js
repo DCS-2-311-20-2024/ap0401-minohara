be19f9687ba5095ad9503df5e395bbd21b1e3101
Takashi MINOHARA <minohara@cs.takushoku-u.ac.jp>
2024-10-15 18:40:44 +0900

    前半終了

--- ap0401.js
+++ ap0401.js
@@ -1,6 +1,6 @@
 //
-// 応用プログラミング 第5回 課題1 (ap0501)
-// G184002021 拓殖太郎
+// 応用プログラミング 第4回 課題 (ap0401)
+// G384002023 蓑原 隆
 //
 "use strict"; // 厳格モード
 
@@ -15,7 +15,7 @@ function init() {
     x: 0,
     y: 25,
     z: 10,
-    axes: false,
+    axes: true,
   };
 
   // シーン作成
@@ -53,6 +53,14 @@ function init() {
   let vz = -Math.cos(pi / 4);
 
   function moveBall(delta) {
+    if (ballLive) {
+      vBall.set(vx, 0, vz)
+      ball.position.addScaledVector(vBall, delta * speed);
+    }
+    else {
+      ball.position.x = paddle.position.x;
+      ball.position.z = paddle.position.z - paddleR - ballR;
+    }
   }
 
   // ボールの死活
@@ -61,10 +69,14 @@ function init() {
 
   // ボールを停止する
   function stopBall() {
+    speed = 0
+    ballLive = false;
   }
 
   // ボールを動かす
   function startBall() {
+    ballLive = true;
+    speed = 10;
   }
 
   // マウスクリックでスタートする
@@ -94,9 +106,12 @@ function init() {
       new THREE.BoxGeometry(vFrameW, vFrameH, vFrameD),
       new MeshPhongMaterial({ color: 0xB3B3B3 })
     );
-
+    lFrame.position.x = (hFrameW - vFrameW)/2;
+    scene.add(lFrame);
     //   右の枠
-
+    const rFrame = lFrame.clone();
+    rFrame.position.x = -(hFrameW - vFrameW)/2;
+    scene.add(rFrame);
   }
 
   // 壁で反射させる
@@ -104,13 +119,26 @@ function init() {
   const vLimit = vFrameD / 2;
   function frameCheck() {
     // 右
-
+    if (ball.position.x + ballR > hLimit) {
+      ball.position.x = hLimit - ballR;
+      vx = -Math.abs(vx);
+    }
     // 左
-
+    if (ball.position.x - ballR < -hLimit) {
+      ball.position.x = -hLimit + ballR;
+      vx = Math.abs(vx);
+    }
     // 上
-
+    if (ball.position.z - ballR < -vLimit) {
+      ball.position.z = -vLimit + ballR;
+      vz = Math.abs(vz);
+    }
     // 下
-
+    if (ball.position.z + ballR > vLimit) {
+      //ball.position.z = vLimit - ballR;
+      //vz = -Math.abs(vz);
+      stopBall();
+    }
   }
 
   // パドル ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
@@ -122,22 +150,29 @@ function init() {
     // パドル中央
     const center = new THREE.Mesh(
       new THREE.CylinderGeometry(paddleR, paddleR, paddleL, nSeg),
-      new THREE.MeshPhongMaterial({ color: 0x333333, specular: 0x404040 })
+      new THREE.MeshPhongMaterial({ color: 0x333333, shininess: 100, specular: 0x404040 })
     );
-
+    center.rotation.z = Math.PI/2;
+    paddle.add(center);
     // パドル端
     const sideGeometry
       = new THREE.SphereGeometry(paddleR, nSeg, nSeg, Math.PI / 2, Math.PI);
     const sideMaterial
-      = new THREE.MeshPhongMaterial({ color: 0x666666, specular: 0xa0a0a0 })
+      = new THREE.MeshPhongMaterial({ color: 0x666666, shininess: 100, specular: 0xa0a0a0 })
     // パドル端(右)
-
+    const right = new THREE.Mesh(sideGeometry, sideMaterial);
+    right.position.x = paddleL/2;
+    paddle.add(right);
     // パドル端(左)
+    const left = right.clone();
+    left.rotation.z = Math.PI;
+    left.position.x = -paddleL/2;
+    paddle.add(left)
 
     // パドルの配置
-
+    paddle.position.z = 0.4 * vFrameD;
+    scene.add(paddle);
   }
-  
 
   // パドル操作
   {
@@ -146,14 +181,37 @@ function init() {
     const raycaster = new THREE.Raycaster();
     const intersects = new THREE.Vector3();
     function paddleMove(event) {
-  
+      mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      raycaster.setFromCamera(mouse, camera);
+      raycaster.ray.intersectPlane(plane, intersects);
+      const offset = hFrameW / 2 - vFrameW - paddleL / 2 - paddleR;
+      if (intersects.x < -offset) {
+        intersects.x = -offset;
+      }
+      else if (intersects.x > offset) {
+        intersects.x = offset;
+      }
+      paddle.position.x = intersects.x;
     }
     window.addEventListener("mousemove", paddleMove, false);
   }
 
   // パドルの衝突検出
   function paddleCheck() {
-  
+    if (Math.abs(ball.position.z - paddle.position.z) < paddleR + ballR &&
+        Math.abs(ball.position.x - paddle.position.x) < paddleL/2 + ballR) {
+      // 中央部分と衝突
+      if (ball.position.z < paddle.position.z) {
+        vz = -Math.abs(vz);
+      }
+      // 右側部分と衝突
+      if (ball.position.x > paddle.position.x + paddleL/2) {
+        vx = Math.abs(vx);
+      }
+      else if (ball.position.x < paddle.position.x - paddleL/2) {
+        vx = -Math.abs(vx);
+      }
+    }
   }
 
   // ブロック ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
@@ -189,7 +247,7 @@ function init() {
   }
 
   // 光源の設定
-  const light = new THREE.SpotLight(0xffffff, 1800);
+  const light = new THREE.SpotLight(0xffffff, 1000);
   light.position.set(0, 15, -10);
   scene.add(light);
 
